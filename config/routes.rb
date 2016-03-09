ba01683a381ac83fdb20dddf043b93ed1206f2c5
Febry Hadinata <febry.hadinata@gmail.com>
2016-03-09 15:07:27 +0000

    new feature microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :microposts do 
+    collection do
+      get 'get_all_microposts'
+    end
+  end
+  
   resources :users do
     collection do
       get 'get_all_users'

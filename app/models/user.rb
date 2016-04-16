55ec03fa28e7091fe9fa5a1c39e9f51a2b56029e
Febry Hadinata <febry.hadinata@gmail.com>
2016-04-17 01:34:58 +0800

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,6 @@
 class User < ActiveRecord::Base
   has_many :microposts
+
   validates :name, presence: true
   validates :email, presence: true
-
-  attr_accessor :name, :email
-
-  def initialize(attributes = {})
-    @name = attributes[:name]
-    @email = attributes[:email]
-  end
-
-  def formatted_email
-    "#{@name} <#{@email}>"
-  end
 end
